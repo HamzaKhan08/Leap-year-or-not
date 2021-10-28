95373b5f2366a84e50d8e7678e543feb939f1040
Hamza Khan <87969486+HamzaKhan08@users.noreply.github.com>
2021-10-28 19:30:27 +0530

    Add files via upload
    hello everyone...
    This code is for finding whether the year is leap or not...
    By my source code you can easily find any year whether its a leap or not..
    
    Thank you
    Regard's 
    HAMZA KHAN

---
+++ index.c
@@ -0,0 +1,23 @@
+/* check whether a year is leap or not */
+#include <stdio.h>
+int main()
+{
+    int yr;
+    printf("\nEnter a year:");
+    scanf("%d", &yr);
+    if (yr % 100 == 0)
+    {
+        if (yr % 400 == 0)
+            printf("Leap year\n");
+        else
+            printf("Not a leap year\n");
+    }
+    else
+    {
+        if (yr % 4 == 0)
+            printf("Leap year\n");
+        else
+            printf("Not a leap year\n");
+    }
+    return 0;
+}
